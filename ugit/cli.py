9fd049da1d13a87f949d334a971ede1c1d7d01ae
yidadaa <yidadaa@qq.com>
2021-08-04 01:51:16 +0800

    finish(add-base-module): finsh section add-base-module
    link: https://www.leshenko.net/p/ugit/#add-base-module

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,6 +1,8 @@
 import argparse
+import sys
 import os
 
+from . import base
 from . import data
 
 
@@ -22,6 +24,10 @@ def parse_args():
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
 
+    cat_file_object_parser = commands.add_parser('cat-file')
+    cat_file_object_parser.set_defaults(func=cat_file)
+    cat_file_object_parser.add_argument('object')
+
     return parser.parse_args()
 
 
@@ -33,3 +39,8 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- ugit/data.py
+++ ugit/data.py
@@ -9,7 +9,22 @@ def init():
     os.makedirs(f'{GIT_DIR}/objects')
 
 
-def hash_object(data):
-    oid = hashlib.sha1(data).hexdigest()
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
-        out.write(data)
+        out.write(obj)
+    return oid
+
+
+def get_object(oid, expected='blob'):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        obj = f.read()
+
+    type_, _, content = obj.partition(b'\x00')
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
+
+    return content
