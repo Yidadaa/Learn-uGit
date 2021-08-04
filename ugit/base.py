77a621766a1012b08581f298be9cd7039c848acc
yidadaa <yidadaa@qq.com>
2021-08-05 01:28:43 +0800

    finish(write-tree-ignore-ugit): link https://www.leshenko.net/p/ugit/#write-tree-ignore-ugit

--- ugit/base.py
+++ ugit/base.py
@@ -8,8 +8,14 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 # TODO: write the file to object store
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
+
+
+def is_ignored(path):
+    return '.ugit' in path.split('/')
--- ugit/cli.py
+++ ugit/cli.py
@@ -50,4 +50,4 @@ def cat_file(args):
 
 
 def write_tree(args):
-    base.write_tree(args)
+    base.write_tree()
