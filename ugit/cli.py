b77b3bcb0542469fd476e41d2596c0c0df33f1a5
yidadaa <yidadaa@qq.com>
2021-08-06 02:03:06 +0800

    finish(log-start-from-oid): finish https://www.leshenko.net/p/ugit/#log-start-from-oid

--- ugit/cli.py
+++ ugit/cli.py
@@ -42,6 +42,7 @@ def parse_args():
 
     log_parser = commands.add_parser('log')
     log_parser.set_defaults(func=log)
+    log_parser.add_argument('oid', nargs='?')
 
     return parser.parse_args()
 
@@ -75,7 +76,7 @@ def commit(args):
 
 
 def log(args):
-    oid = data.get_HEAD()
+    oid = args.oid or data.get_HEAD()
     while oid:
         commit = base.get_commit(oid)
         print(f'commit {oid}\n')
