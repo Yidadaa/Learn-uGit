6d16ac58af1ebca91bd2cb81b3a4ec626daf3e3b
yidadaa <yidadaa@qq.com>
2021-08-06 01:33:19 +0800

    finish(read-tree-cleanup): finish https://www.leshenko.net/p/ugit/#read-tree-cleanup

--- ugit/base.py
+++ ugit/base.py
@@ -50,8 +50,21 @@ def get_tree(oid, base_path=''):
     return result
 
 
+def _empty_current_directory():
+    for root, dirnames, filenames in os.walk('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath(f'{root}/{filename}')
+            if is_ignored(path) or not os.path.isfile(path):
+                continue
+            try:
+                os.rmdir(path)
+            except (FileNotFoundError, OSError):
+                pass
+
+
 def read_tree(tree_oid):
     '''Get tree and write tree back to directory.'''
+    _empty_current_directory()
     for path, oid in get_tree(tree_oid, base_path='./').items():
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'wb') as f:
