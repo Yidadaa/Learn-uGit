94cc9859bacf7c97883d102a47ec058eb352430a
yidadaa <yidadaa@qq.com>
2021-08-11 01:32:22 +0800

    feat(k-iterable-commits): https://www.leshenko.net/p/ugit/#k-iterate-commits

--- ugit/base.py
+++ ugit/base.py
@@ -118,6 +118,20 @@ def get_commit(oid):
     return Commit(tree=tree, parent=parent, message=message)
 
 
+def iter_commits_and_parents(oids):
+    oids = set(oids)
+    visited = set()
+
+    while oids:
+        oid = oids.pop()
+        if not oid or oid in visited:
+            continue
+        visited.add(oid)
+        yield oid
+        commit = get_commit(oid)
+        oids.add(commit.parent)
+
+
 def get_oid(name):
     # alias @ as HEAD
     if name == '@':
--- ugit/cli.py
+++ ugit/cli.py
@@ -110,5 +110,13 @@ def tag(args):
 
 
 def k(args):
+    oids = set()
     for refname, ref in data.iter_refs():
         print(refname, ref)
+        oids.add(ref)
+
+    for oid in base.iter_commits_and_parents(oids):
+        commit = base.get_commit(oid)
+        print(oid)
+        if commit.parent:
+            print('Parent', commit.parent)
