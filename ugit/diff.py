94fdf96130576d00adfb41ee22221a13c54c280a
yidadaa <yidadaa@qq.com>
2021-08-19 01:56:57 +0800

    finish(show-diff): https://www.leshenko.net/p/ugit/#show-diff

--- ugit/diff.py
+++ ugit/diff.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+import difflib
+
+from . import data
 
 
 def compare_trees(*trees):
@@ -16,4 +19,19 @@ def diff_trees(t_from, t_to):
     for path, o_from, o_to in compare_trees(t_from, t_to):
         if o_from != o_to:
             output += f'changed: {path}\n'
+            diff_result = diff_blobs(o_from, o_to)
+            output += f'{diff_result}\n\n' if diff_result else ''
     return output
+
+
+def diff_blobs(o_from, o_to, path='blob'):
+    try:
+        b_from = data.get_object(o_from).decode(errors='ignore')
+    except:
+        b_from = ''
+    try:
+        b_to = data.get_object(o_to).decode(errors='ignore')
+    except:
+        b_to = ''
+    result = difflib.unified_diff(b_from.splitlines(), b_to.splitlines())
+    return '\n'.join(result)
