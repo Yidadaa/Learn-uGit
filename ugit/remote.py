0feeae4e9c8903368c5a35f2f6f54c464e54fa20
yidadaa <yidadaa@qq.com>
2021-08-31 01:22:33 +0800

    finish(print-remote-refs-refactor): https://www.leshenko.net/p/ugit/#print-remote-refs-refactor

--- ugit/remote.py
+++ ugit/remote.py
@@ -3,6 +3,10 @@ from . import data  # pylint: disable=relative-beyond-top-level
 
 def fetch(remote_path):
     print('Will fetch the following refs:')
+    for refname, _ in _get_remote_refs(remote_path, 'refs/heads').items():
+        print(f'- {refname}')
+
+
+def _get_remote_refs(remote_path, prefix=''):
     with data.change_git_dir(remote_path):
-        for refname, _ in data.iter_refs('refs/heads'):
-            print(f'- {refname}')
+        return {refname: ref.value for refname, ref in data.iter_refs(prefix)}
