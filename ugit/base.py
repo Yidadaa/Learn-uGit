413261816eb5161103a40f00aaa9ec60d1cabfc0
yidadaa <yidadaa@qq.com>
2021-09-02 01:30:35 +0800

    finish(write-tree-from-index): https://www.leshenko.net/p/ugit/#write-tree-from-index

--- ugit/base.py
+++ ugit/base.py
@@ -15,25 +15,33 @@ def init():
         symbolic=True, value='refs/heads/master'))
 
 
-def write_tree(directory='.'):
-    '''Write directory to store recurisvely.'''
-    entries = []
-    with os.scandir(directory) as it:
-        for entry in it:
-            full = f'{directory}/{entry.name}'
-            if is_ignored(full):
-                continue
-            if entry.is_file(follow_symlinks=False):
-                type_ = 'blob'
-                with open(full, 'rb') as f:
-                    oid = data.hash_object(f.read())
-            elif entry.is_dir(follow_symlinks=False):
+def write_tree():
+    index_as_tree = {}
+    with data.get_index() as index:
+        for path, oid in index.items():
+            path = path.split('/')
+            dirpath, filename = path[:-1], path[-1]
+
+            # convert flat index to tree-like index
+            current = index_as_tree
+            for dirname in dirpath:
+                current = current.setdefault(dirname, {})
+            current[filename] = oid
+
+    def write_tree_recursive(tree_dict: dict):
+        entries = []
+        for name, value in tree_dict.items():
+            if type(value) is dict:
                 type_ = 'tree'
-                oid = write_tree(full)
-            entries.append((entry.name, oid, type_))
-    tree = ''.join(f'{type_} {oid} {name}\n' for name,
-                   oid, type_ in sorted(entries))
-    return data.hash_object(tree.encode(), 'tree')
+                oid = write_tree_recursive(value)
+            else:
+                type_ = 'blob'
+                oid = value
+            entries.append((name, oid, type_))
+
+        tree = ''.join(f'{type_} {oid} {name}\n' for name,
+                       oid, type_ in sorted(entries))
+        return data.hash_object(tree.encode(), 'tree')
 
 
 def _iter_tree_entries(oid):
